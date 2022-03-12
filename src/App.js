aece45c35aae49bb571ca2a4805480e5023080f7
gauravsewani <lavishgps@gmail.com>
2022-03-12 16:02:04 +0530

    almost completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import About from "./components/About";
+import AllInOne from "./components/AllInOne";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 function NavBar() {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => setNav(!nav);
+
+  const handleClose = () => setNav(!nav);
+
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">Misty.</h1>
           <ul className="hidden md:flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Support</li>
-            <li>Platforms</li>
-            <li>Pricing</li>
+            <li>
+              <Link to="home" smooth={true} duration={500}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="about" smooth={true} offset={-200} duration={500}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="support" smooth={true} offset={-50} duration={500}>
+                Support
+              </Link>
+            </li>
+            <li>
+              <Link to="platform" smooth={true} offset={-100} duration={500}>
+                Platforms
+              </Link>
+            </li>
+            <li>
+              <Link to="Pricing" smooth={true} offset={50} duration={500}>
+                Pricing
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="hidden md:flex pr-4">
@@ -22,8 +49,78 @@ function NavBar() {
           </button>
           <button className="px-8 py-3">Log Up</button>
         </div>
+        <div className="md:hidden mr-4" onClick={handleClick}>
+          {!nav ? <MenuIcon className="w-5 " /> : <XIcon className="w-5" />}
+        </div>
       </div>
-      <MenuIcon className="w-5 " />
+
+      <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+        <li>
+          <Link
+            onClick={handleClose}
+            className="border-b-2 border-zinc-300 w-full"
+            to="home"
+            smooth={true}
+            duration={500}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={handleClose}
+            className="border-b-2 border-zinc-300 w-full"
+            to="about"
+            smooth={true}
+            offset={-200}
+            duration={500}
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={handleClose}
+            className="border-b-2 border-zinc-300 w-full"
+            to="support"
+            smooth={true}
+            offset={-50}
+            duration={500}
+          >
+            Support
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={handleClose}
+            className="border-b-2 border-zinc-300 w-full"
+            to="platform"
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Platforms
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={handleClose}
+            className="border-b-2 border-zinc-300 w-full"
+            to="Pricing"
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            Pricing
+          </Link>
+        </li>
+        <div className="flex flex-col mr-4">
+          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4 ">
+            Sign In
+          </button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+      </ul>
     </div>
   );
 }
